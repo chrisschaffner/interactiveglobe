e4215b09a955d4c3cf8ee63f1ad3b7e499f4c33b
C.Schaffner <c.schaffner@campus.lmu.de>
2023-12-28 23:11:37 +0100

    Fixed path to three.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-import * as THREE from '/three';
-import { MeshPhongMaterial } from 'three';
+import * as THREE from 'node_modules/three/build/three.js';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
