4c72e334d7e08bd505bd521274800117db881911
C.Schaffner <c.schaffner@campus.lmu.de>
2023-12-29 14:42:10 +0100

    Fixed path to three.js

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { MeshPhongMaterial } from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
 import { OutlinePass } from "three/examples/jsm/postprocessing/OutlinePass.js";
